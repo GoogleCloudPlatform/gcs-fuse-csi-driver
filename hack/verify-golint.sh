89b92840cffbcbf6539108e462f3d362c6e3a855
Jiaxun Song <songjiaxun@google.com>
2023-06-18 00:50:22 +0000

    update go builder

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -32,6 +32,6 @@ golangci-lint run --no-config --deadline=10m --sort-results \
 --enable-all  \
 --max-same-issues 100 \
 --disable maligned,varcheck,nosnakecase,golint,scopelint,interfacer,deadcode,ifshort,structcheck,exhaustivestruct,exhaustruct,gomnd,lll,gochecknoglobals,funlen,varnamelen,wsl,testpackage,wrapcheck,goerr113,ireturn,gocyclo,cyclop,godox,gocognit,nestif,gomoddirectives \
---go 1.20.4 # the builder verison
+--go 1.20.5 # the builder verison
 
 echo "Congratulations! Lint check completed for all Go source files."
