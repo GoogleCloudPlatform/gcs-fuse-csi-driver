e7b833412bd27d3fd469ac1bb0dfa430ffb958db
Jiaxun Song <songjiaxun@google.com>
2023-09-20 19:42:54 +0000

    skip gcsfuse implicit_dir integration test until the next release

--- test/e2e/utils/handler.go
+++ test/e2e/utils/handler.go
@@ -198,7 +198,7 @@ func generateTestSkip(testParams *TestParameters) string {
 
 	// TODO(songjiaxun) remove this logic after the next gcsfusee release.
 	if testParams.UseGKEManagedDriver {
-		skipTests = append(skipTests, "gzip")
+		skipTests = append(skipTests, "gzip", "implicit_dir")
 	}
 
 	skipString := strings.Join(skipTests, "|")
