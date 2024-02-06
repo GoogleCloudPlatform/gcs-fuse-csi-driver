b07959ae2ce3c66cd6a86f6a117c67034ef0390f
Jiaxun Song <songjiaxun@google.com>
2024-02-06 05:06:56 +0000

    disable custom buffer e2e tests in prod

--- test/e2e/utils/handler.go
+++ test/e2e/utils/handler.go
@@ -199,7 +199,7 @@ func generateTestSkip(testParams *TestParameters) string {
 
 	// TODO(songjiaxun) remove this logic after the next CSI driver release.
 	if testParams.UseGKEManagedDriver {
-		skipTests = append(skipTests, "Deployment.while.scaling.down", "Pod.is.terminating", "RestartPolicy.OnFailure")
+		skipTests = append(skipTests, "custom.buffer", "Deployment.while.scaling.down", "Pod.is.terminating", "RestartPolicy.OnFailure")
 	}
 
 	skipString := strings.Join(skipTests, "|")
