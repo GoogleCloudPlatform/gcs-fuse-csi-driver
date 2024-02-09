028a3d7f09c25f8ddeeeda13b39bfa2399324b98
Jiaxun Song <songjiaxun@google.com>
2024-02-09 20:09:41 +0000

    add fio loading test examples

--- examples/dlio/parse_logs.py
+++ examples/dlio/parse_logs.py
@@ -1,5 +1,6 @@
-import sys, subprocess, os, json
+import subprocess, os, json
 import pprint
+from typing import Tuple
 
 LOCAL_LOGS_LOCATION = "../../bin/dlio-logs"
 
@@ -16,22 +17,60 @@ record = {
     "end": "",
     "highest_memory": 0,
     "lowest_memory": 0,
+    "highest_cpu": 0.0,
+    "lowest_cpu": 0.0,
 }
 
-def get_memory(pod_name: str, start: str, end: str) -> (int, int):
-    start = start.split('.')[0].replace('T', ' ') + " UTC"
-    end = end.split('.')[0].replace('T', ' ') + " UTC"
-    print(pod_name, start, end)
+def get_memory(pod_name: str, start: str, end: str) -> Tuple[int, int]:
+    # for some reason, the mash filter does not always work, so we fetch all the metrics for all the pods and filter later.
     result = subprocess.run(["mash", "--namespace=cloud_prod", "--output=csv", 
-                             f"Query(Fetch(Raw('cloud.kubernetes.K8sContainer', 'kubernetes.io/container/memory/used_bytes'), {{'cluster_name': 'gcsfuse-csi-test-cluster', 'container_name': 'gke-gcsfuse-sidecar', 'pod_name': '{pod_name}', 'project': '641665282868', 'metric:memory_type': 'non-evictable'}})| Window(Align('10m'))| GroupBy(['container_name'], Sum()), TimeInterval('{start}', '{end}'), '5s')"], 
+                             f"Query(Fetch(Raw('cloud.kubernetes.K8sContainer', 'kubernetes.io/container/memory/used_bytes'), {{'project': '641665282868', 'metric:memory_type': 'non-evictable'}})| Window(Align('10m'))| GroupBy(['pod_name', 'container_name'], Max()), TimeInterval('{start}', '{end}'), '5s')"], 
                              capture_output=True, text=True)
 
-    data_points = result.stdout.split("\n")[1].split(",")[7:]
-    data_points_int = [int(d) for d in data_points]
+    data_points_int = []
+    data_points_by_pod_container = result.stdout.strip().split("\n")
+    for data_points in data_points_by_pod_container[1:]:
+        data_points_split = data_points.split(",")
+        pn = data_points_split[4]
+        container_name = data_points_split[5]
+        if pn == pod_name and container_name == "gke-gcsfuse-sidecar":
+            try:
+                data_points_int = [int(d) for d in data_points_split[7:]]
+            except:
+                print(f"failed to parse memory for pod {pod_name}, {start}, {end}, data {data_points_int}")
+            break
     if not data_points_int:
         return 0, 0
     
-    return int(min(data_points_int) / 1024 / 1024) , int(max(data_points_int) / 1024 / 1024)
+    return int(min(data_points_int) / 1024 ** 2) , int(max(data_points_int) / 1024 ** 2)
+
+def get_cpu(pod_name: str, start: str, end: str) -> Tuple[float, float]:
+    # for some reason, the mash filter does not always work, so we fetch all the metrics for all the pods and filter later.
+    result = subprocess.run(["mash", "--namespace=cloud_prod", "--output=csv", 
+                             f"Query(Fetch(Raw('cloud.kubernetes.K8sContainer', 'kubernetes.io/container/cpu/core_usage_time'), {{'project': '641665282868'}})| Window(Rate('10m'))| GroupBy(['pod_name', 'container_name'], Max()), TimeInterval('{start}', '{end}'), '5s')"], 
+                             capture_output=True, text=True)
+
+    data_points_float = []
+    data_points_by_pod_container = result.stdout.split("\n")
+    for data_points in data_points_by_pod_container[1:]:
+        data_points_split = data_points.split(",")
+        pn = data_points_split[4]
+        container_name = data_points_split[5]
+        if pn == pod_name and container_name == "gke-gcsfuse-sidecar":
+            try:
+                data_points_float = [float(d) for d in data_points_split[6:]]
+            except:
+                print(f"failed to parse CPU for pod {pod_name}, {start}, {end}, data {data_points_float}")
+            
+            break
+    
+    if not data_points_float:
+        return 0.0, 0.0
+    
+    return round(min(data_points_float), 5) , round(max(data_points_float), 5)
+
+def standard_timestamp(timestamp: int) -> str:
+    return timestamp.split('.')[0].replace('T', ' ') + " UTC"
 
 if __name__ == "__main__":
     bucketNames = ["gke-dlio-unet3d-100kb-500k", "gke-dlio-unet3d-150mb-5k", "gke-dlio-unet3d-3mb-100k", "gke-dlio-unet3d-500kb-1m"]
@@ -94,9 +133,11 @@ if __name__ == "__main__":
                 r["duration"] = int(float(per_epoch_stats_data[str(i+1)]["duration"]))
                 r["train_throughput_samples_per_second"] = int(summary_data["metric"]["train_throughput_samples_per_second"][i])
                 r["train_throughput_mb_per_second"] = int(r["train_throughput_samples_per_second"] * int(output[key]["mean_file_size"]) / (1024 ** 2))
-                r["start"] = per_epoch_stats_data[str(i+1)]["start"]
-                r["end"] = per_epoch_stats_data[str(i+1)]["end"]
-                r["lowest_memory"], r["highest_memory"] = get_memory(r["pod_name"], r["start"], r["end"])
+                r["start"] = standard_timestamp(per_epoch_stats_data[str(i+1)]["start"])
+                r["end"] = standard_timestamp(per_epoch_stats_data[str(i+1)]["end"])
+                if r["scenario"] != "local-ssd":
+                    r["lowest_memory"], r["highest_memory"] = get_memory(r["pod_name"], r["start"], r["end"])
+                    r["lowest_cpu"], r["highest_cpu"] = get_cpu(r["pod_name"], r["start"], r["end"])
 
                 pprint.pprint(r)
 
@@ -109,7 +150,7 @@ if __name__ == "__main__":
     scenario_order = ["local-ssd", "gcsfuse-no-file-cache", "gcsfuse-file-cache"]
 
     output_file = open("./output.csv", "a")
-    output_file.write("File Size,File #,Total Size (GB),Batch Size,Scenario,Epoch,Duration (s),GPU Utilization (%),Throughput (sample/s),Throughput (MB/s),Throughput over Local SSD (%),GCSFuse Lowest Memory (MB),GCSFuse Highest Memory (MB),Pod,Start,End\n")
+    output_file.write("File Size,File #,Total Size (GB),Batch Size,Scenario,Epoch,Duration (s),GPU Utilization (%),Throughput (sample/s),Throughput (MB/s),Throughput over Local SSD (%),GCSFuse Lowest Memory (MB),GCSFuse Highest Memory (MB),GCSFuse Lowest CPU (core),GCSFuse Highest CPU (core),Pod,Start,End\n")
     
     for key in output_order:
         if key not in output:
@@ -122,6 +163,6 @@ if __name__ == "__main__":
                 r = record_set["records"][scenario][i]
                 r["throughput_over_local_ssd"] = round(r["train_throughput_mb_per_second"] / record_set["records"]["local-ssd"][i]["train_throughput_mb_per_second"] * 100, 2)
                 output_file.write(f"{record_set['mean_file_size']},{record_set['num_files_train']},{total_size},{record_set['batch_size']},{scenario},")
-                output_file.write(f"{r['epoch']},{r['duration']},{r['train_au_percentage']},{r['train_throughput_samples_per_second']},{r['train_throughput_mb_per_second']},{r['throughput_over_local_ssd']},{r['lowest_memory']},{r['highest_memory']},{r['pod_name']},{r['start']},{r['end']}\n")
+                output_file.write(f"{r['epoch']},{r['duration']},{r['train_au_percentage']},{r['train_throughput_samples_per_second']},{r['train_throughput_mb_per_second']},{r['throughput_over_local_ssd']},{r['lowest_memory']},{r['highest_memory']},{r['lowest_cpu']},{r['highest_cpu']},{r['pod_name']},{r['start']},{r['end']}\n")
 
     output_file.close()
---
+++ examples/fio/README.md
@@ -0,0 +1,52 @@
+# FIO Loading Tests (TODO: current doc is for DLIO, modify it for FIO)
+
+## Prerequisites
+
+### Create a new node pool
+
+For an existing GKE cluster, use the following command to create a new node pool. Make sure the cluster has the [Workload Identity feature enabled](https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity#enable).
+
+> In this early stage test, the managed GCS FUSE CSI driver feature is disabled, and the driver is manually installed.
+
+```bash
+# Replace the cluster name and zone.
+gcloud container node-pools create large-pool \
+    --cluster cluster-1-29-us-west1 \
+    --ephemeral-storage-local-ssd count=16 \
+    --machine-type n2-standard-96 \
+    --zone us-west1-c \
+    --num-nodes 3
+```
+
+### Set up GCS bucket
+
+Create a GCS bucket using `Location type`: `Region`, and select the same region where your cluster runs. Follow the [GKE documentation](https://cloud.google.com/kubernetes-engine/docs/how-to/persistent-volumes/cloud-storage-fuse-csi-driver#authentication) to configure the access. This example uses the default Kubernetes service account in the default Kubernetes namespace.
+
+### Install Helm
+
+The example uses Helm charts to manage the applications. Follow the [Helm documentation](https://helm.sh/docs/intro/install/#from-script) to install Helm.
+
+## FIO Loading Tests
+
+Change the directory to `./examples/fio`. Run the following commands to run the loading tests. Each `helm install` command will deploy a Pod to run the test, and upload logs to the bucket.
+
+### Run the tests
+
+```bash
+python ./run_tests.py
+```
+
+### Delete the tests
+
+```bash
+python ./delete_tests.py
+```
+
+## Parsing the test results
+
+Run the following python script to parse the logs. The results will be saved in `./examples/fio/output.csv`.
+
+```bash
+cd ./examples/fio
+python ./parse_logs.py
+```
