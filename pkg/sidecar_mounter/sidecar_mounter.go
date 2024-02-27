93444c32f002ab3aaeeb9336fef2ed5eb108f996
Jiaxun Song <songjiaxun@google.com>
2024-02-27 05:24:10 +0000

    refactor sidecar mounter config

--- pkg/sidecar_mounter/sidecar_mounter.go
+++ pkg/sidecar_mounter/sidecar_mounter.go
@@ -18,27 +18,12 @@ limitations under the License.
 package sidecarmounter
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"net"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strconv"
-	"strings"
-	"syscall"
 
-	"github.com/googlecloudplatform/gcs-fuse-csi-driver/pkg/util"
-	"github.com/googlecloudplatform/gcs-fuse-csi-driver/pkg/webhook"
-	"gopkg.in/yaml.v3"
 	"k8s.io/klog/v2"
-)
-
-const (
-	GCSFuseAppName = "gke-gcs-fuse-csi"
-	TempDir        = "/temp-dir"
 )
 
 // Mounter will be used in the sidecar container to invoke gcsfuse.
@@ -56,18 +41,6 @@ func New(mounterPath string) *Mounter {
 	}
 }
 
-// MountConfig contains the information gcsfuse needs.
-type MountConfig struct {
-	FileDescriptor int       `json:"-"`
-	VolumeName     string    `json:"volumeName,omitempty"`
-	BucketName     string    `json:"bucketName,omitempty"`
-	BufferDir      string    `json:"-"`
-	CacheDir       string    `json:"-"`
-	ConfigFile     string    `json:"-"`
-	Options        []string  `json:"options,omitempty"`
-	ErrWriter      io.Writer `json:"-"`
-}
-
 func (m *Mounter) Mount(mc *MountConfig) (*exec.Cmd, error) {
 	klog.Infof("start to mount bucket %q for volume %q", mc.BucketName, mc.VolumeName)
 
@@ -75,14 +48,9 @@ func (m *Mounter) Mount(mc *MountConfig) (*exec.Cmd, error) {
 		return nil, fmt.Errorf("failed to create temp dir %q: %w", mc.BufferDir+TempDir, err)
 	}
 
-	flagMap, configFileFlagMap := mc.prepareMountArgs()
-	if err := mc.prepareConfigFile(configFileFlagMap); err != nil {
-		return nil, fmt.Errorf("failed to create config file %q: %w", mc.ConfigFile, err)
-	}
-
 	args := []string{"gcsfuse"}
 
-	for k, v := range flagMap {
+	for k, v := range mc.FlagMap {
 		args = append(args, "--"+k)
 		if v != "" {
 			args = append(args, v)
@@ -110,204 +78,4 @@ func (m *Mounter) Mount(mc *MountConfig) (*exec.Cmd, error) {
 
 func (m *Mounter) GetCmds() []*exec.Cmd {
 	return m.cmds
-}
-
-var disallowedFlags = map[string]bool{
-	"temp-dir":                             true,
-	"config-file":                          true,
-	"foreground":                           true,
-	"log-file":                             true,
-	"log-format":                           true,
-	"key-file":                             true,
-	"token-url":                            true,
-	"reuse-token-from-url":                 true,
-	"o":                                    true,
-	"logging:file-path":                    true,
-	"logging:format":                       true,
-	"logging:log-rotate:max-file-size-mb":  true,
-	"logging:log-rotate:backup-file-count": true,
-	"logging:log-rotate:compress":          true,
-	"cache-dir":                            true,
-	"experimental-local-file-cache":        true,
-}
-
-var boolFlags = map[string]bool{
-	"implicit-dirs":                 true,
-	"enable-nonexistent-type-cache": true,
-	"debug_fuse_errors":             true,
-	"debug_fuse":                    true,
-	"debug_fs":                      true,
-	"debug_gcs":                     true,
-	"debug_http":                    true,
-	"debug_invariants":              true,
-	"debug_mutex":                   true,
-}
-
-// Fetch the following information from a given socket path:
-// 1. Pod volume name
-// 2. The file descriptor
-// 3. GCS bucket name
-// 4. Mount options passing to gcsfuse (passed by the csi mounter).
-func NewMountConfig(sp string) (*MountConfig, error) {
-	// socket path pattern: /gcsfuse-tmp/.volumes/<volume-name>/socket
-	volumeName := filepath.Base(filepath.Dir(sp))
-	mc := MountConfig{
-		VolumeName: volumeName,
-		BufferDir:  filepath.Join(webhook.SidecarContainerBufferVolumeMountPath, ".volumes", volumeName),
-		CacheDir:   filepath.Join(webhook.SidecarContainerCacheVolumeMountPath, ".volumes", volumeName),
-		ConfigFile: filepath.Join(webhook.SidecarContainerTmpVolumeMountPath, ".volumes", volumeName, "config.yaml"),
-	}
-
-	klog.Infof("connecting to socket %q", sp)
-	c, err := net.Dial("unix", sp)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to the socket %q: %w", sp, err)
-	}
-
-	fd, msg, err := util.RecvMsg(c)
-	if err != nil {
-		return nil, fmt.Errorf("failed to receive mount options from the socket %q: %w", sp, err)
-	}
-	// as we got all the information from the socket, closing the connection and deleting the socket
-	c.Close()
-	if err = syscall.Unlink(sp); err != nil {
-		klog.Errorf("failed to close socket %q: %v", sp, err)
-	}
-
-	mc.FileDescriptor = fd
-
-	if err := json.Unmarshal(msg, &mc); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal the mount config: %w", err)
-	}
-
-	if mc.BucketName == "" {
-		return nil, errors.New("failed to fetch bucket name from CSI driver")
-	}
-
-	return &mc, nil
-}
-
-func (mc *MountConfig) prepareMountArgs() (map[string]string, map[string]string) {
-	flagMap := map[string]string{
-		"app-name":    GCSFuseAppName,
-		"temp-dir":    mc.BufferDir + TempDir,
-		"config-file": mc.ConfigFile,
-		"foreground":  "",
-		"uid":         "0",
-		"gid":         "0",
-	}
-
-	configFileFlagMap := map[string]string{
-		"logging:file-path": "/dev/fd/1", // redirect the output to cmd stdout
-		"logging:format":    "text",
-		"cache-dir":         "", // by default the gcsfuse file cache is disabled on GKE
-	}
-
-	invalidArgs := []string{}
-
-	for _, arg := range mc.Options {
-		if strings.Contains(arg, ":") {
-			i := strings.LastIndex(arg, ":")
-			f, v := arg[:i], arg[i+1:]
-
-			if disallowedFlags[f] {
-				invalidArgs = append(invalidArgs, arg)
-			} else {
-				configFileFlagMap[f] = v
-			}
-
-			// if the value of flag file-cache:max-size-mb is not 0,
-			// enable the file cache feature by passing the cache directory.
-			if f == "file-cache:max-size-mb" && v != "0" {
-				configFileFlagMap["cache-dir"] = mc.CacheDir
-			}
-
-			continue
-		}
-
-		argPair := strings.SplitN(arg, "=", 2)
-		if len(argPair) == 0 {
-			continue
-		}
-
-		flag := argPair[0]
-		if disallowedFlags[flag] {
-			invalidArgs = append(invalidArgs, arg)
-
-			continue
-		}
-
-		value := ""
-		if len(argPair) > 1 {
-			value = argPair[1]
-		}
-
-		switch {
-		case boolFlags[flag] && value != "":
-			flag = flag + "=" + value
-			if value == "true" || value == "false" {
-				value = ""
-			} else {
-				invalidArgs = append(invalidArgs, flag)
-
-				continue
-			}
-		case flag == "app-name":
-			value = GCSFuseAppName + "-" + value
-		}
-
-		flagMap[flag] = value
-	}
-
-	if len(invalidArgs) > 0 {
-		klog.Warningf("got invalid arguments for volume %q: %v. Will discard invalid args and continue to mount.",
-			invalidArgs, mc.VolumeName)
-	}
-
-	return flagMap, configFileFlagMap
-}
-
-func (mc *MountConfig) prepareConfigFile(flagMap map[string]string) error {
-	configMap := map[string]interface{}{}
-
-	for f, v := range flagMap {
-		curLevel := configMap
-		tokens := strings.Split(f, ":")
-		for i, t := range tokens {
-			if i == len(tokens)-1 {
-				if _, ok := curLevel[t].(map[string]interface{}); ok {
-					return fmt.Errorf("invalid config file flag: %q", f)
-				}
-
-				if intVal, err := strconv.ParseInt(v, 10, 64); err == nil {
-					curLevel[t] = intVal
-				} else if boolVal, err := strconv.ParseBool(v); err == nil {
-					curLevel[t] = boolVal
-				} else {
-					curLevel[t] = v
-				}
-
-				break
-			}
-
-			if _, ok := curLevel[t]; !ok {
-				curLevel[t] = map[string]interface{}{}
-			}
-
-			if nextLevel, ok := curLevel[t].(map[string]interface{}); ok {
-				curLevel = nextLevel
-			} else {
-				return fmt.Errorf("invalid config file flag: %q", f)
-			}
-		}
-	}
-
-	yamlData, err := yaml.Marshal(&configMap)
-	if err != nil {
-		return err
-	}
-
-	klog.Infof("gcsfuse config file content:\n%v", string(yamlData))
-
-	return os.WriteFile(mc.ConfigFile, yamlData, 0o400)
 }
