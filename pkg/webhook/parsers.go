867daea211e265c3bd735250d03da4b045f2405f
hime <jaimebz@google.com>
2024-07-24 00:50:08 +0000

    Update Privately Hosted Sidecar Image parser to remove initContainer sidecar declarations.

--- pkg/webhook/parsers.go
+++ pkg/webhook/parsers.go
@@ -27,27 +27,34 @@ import (
 
 var minimumSupportedVersion = version.MustParseGeneric("1.29.0")
 
-// iterates the container list,
-// if a container is named "gke-gcsfuse-sidecar",
-// extract the container image and check if the image is valid,
-// then removes this container from the container list.
+// parseSidecarContainerImage supports our Privately Hosted Sidecar Image option
+// by iterating the container list and finding a container named "gke-gcsfuse-sidecar"
+// If we find "gke-gcsfuse-sidecar":
+//   - extract the container image and check if the image is valid
+//   - removes the container definition from the container list.
+//   - remove any mentions of "gke-gcsfuse-sidecar" from initContainer list.
+//   - return image
 func parseSidecarContainerImage(pod *corev1.Pod) (string, error) {
 	var image string
-	var index int
-	for i, c := range pod.Spec.Containers {
-		if c.Name == SidecarContainerName {
-			image = c.Image
-			index = i
 
-			if _, _, _, err := parsers.ParseImageName(image); err != nil {
-				return "", fmt.Errorf("could not parse input image: %q, error: %w", image, err)
-			}
+	// Find container named "gke-gcsfuse-sidecar" (SidecarContainerName), extract its image, and remove from list.
+	if index, present := containerPresent(pod.Spec.Containers, SidecarContainerName); present {
+		image = pod.Spec.Containers[index].Image
+
+		if _, _, _, err := parsers.ParseImageName(image); err != nil {
+			return "", fmt.Errorf("could not parse input image: %q, error: %w", image, err)
+		}
+
+		if image != "" {
+			copy(pod.Spec.Containers[index:], pod.Spec.Containers[index+1:])
+			pod.Spec.Containers = pod.Spec.Containers[:len(pod.Spec.Containers)-1]
 		}
 	}
 
-	if image != "" {
-		copy(pod.Spec.Containers[index:], pod.Spec.Containers[index+1:])
-		pod.Spec.Containers = pod.Spec.Containers[:len(pod.Spec.Containers)-1]
+	// Remove any mention of gke-gcsfuse-sidecar from init container list.
+	if index, present := containerPresent(pod.Spec.InitContainers, SidecarContainerName); present {
+		copy(pod.Spec.InitContainers[index:], pod.Spec.InitContainers[index+1:])
+		pod.Spec.InitContainers = pod.Spec.InitContainers[:len(pod.Spec.InitContainers)-1]
 	}
 
 	return image, nil
