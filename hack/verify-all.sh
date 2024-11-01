6a6603196a50935cae4b6459f84d56cee0461585
hime <jaimebz@google.com>
2024-11-01 22:41:31 +0000

    Remove boilerplate check for future files.

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -20,7 +20,6 @@ set -o pipefail
 
 PKG_ROOT=$(git rev-parse --show-toplevel)
 
-${PKG_ROOT}/hack/verify-boilerplate.sh
 ${PKG_ROOT}/hack/verify-gofmt.sh
 ${PKG_ROOT}/hack/verify-govet.sh
 ${PKG_ROOT}/hack/verify-golint.sh
