3692889ca32766014427dc5e4d2abcde1546a049
hime <jaimebz@google.com>
2025-02-14 17:52:08 +0000

    Revert "Bump k8s.io/kubernetes from 1.30.3 to 1.30.10"
    Change-Id: I3533ccb7c1ffaef26a88721e9dd56328aa61da9f

--- vendor/k8s.io/kubernetes/pkg/features/kube_features.go
+++ vendor/k8s.io/kubernetes/pkg/features/kube_features.go
@@ -541,8 +541,7 @@ const (
 	// alpha: v1.27
 	// beta: v1.30
 	//
-	// Enables querying logs of node services using the /logs endpoint. Enabling this feature has security implications.
-	// The recommendation is to enable it on a need basis for debugging purposes and disabling otherwise.
+	// Enables querying logs of node services using the /logs endpoint
 	NodeLogQuery featuregate.Feature = "NodeLogQuery"
 
 	// owner: @xing-yang @sonasingh46
@@ -1268,7 +1267,7 @@ var defaultKubernetesFeatureGates = map[featuregate.Feature]featuregate.FeatureS
 
 	genericfeatures.APIServerTracing: {Default: true, PreRelease: featuregate.Beta},
 
-	genericfeatures.APIServingWithRoutine: {Default: false, PreRelease: featuregate.Alpha},
+	genericfeatures.APIServingWithRoutine: {Default: true, PreRelease: featuregate.Beta},
 
 	genericfeatures.ConsistentListFromCache: {Default: false, PreRelease: featuregate.Alpha},
 
