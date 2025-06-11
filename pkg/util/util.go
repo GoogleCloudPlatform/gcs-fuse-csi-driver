f6527b0547b4f01962e73099247c335adf21b497
hime <jaimebz@google.com>
2025-06-11 23:15:24 +0000

    Clean up node restart support logging and comments.
    Change-Id: Ieceadd4fabe1cced2f904289641bd770b711d56c

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -201,7 +201,7 @@ func CheckAndDeleteStaleFile(dirPath, fileName string) error {
 	if deleteErr := os.Remove(filePath); deleteErr != nil {
 		return fmt.Errorf("failed to delete file '%s': %w", filePath, deleteErr)
 	}
-	klog.Infof("Stale file '%s' successfully deleted.", fileName)
+	klog.Infof("Stale file '%s' successfully deleted", fileName)
 
 	return nil
 }
