6b05ec288e6dc175b7a1e326c738f99a1a90b166
Sneha Aradhey <snehaaradhey@google.com>
2025-11-10 18:53:48 +0000

    Update supported minimum GKE version for sidecar bucket access check feature

--- test/e2e/utils/cluster.go
+++ test/e2e/utils/cluster.go
@@ -37,7 +37,7 @@ var (
 	metadataPrefetchMinimumVersion              = version.MustParseGeneric("1.32.0")
 	longMountOptionsMinimumVersion              = version.MustParseGeneric("1.32.0")
 	supportsMachineTypeAutoConfigMinimumVersion = version.MustParseGeneric("1.33.0")
-	sidecarBucketAccessCheckMinimumVersion      = version.MustParseGeneric("1.34.0")
+	sidecarBucketAccessCheckMinimumVersion      = version.MustParseGeneric("1.34.1-gke.2931000")
 )
 
 // gcloudCommand constructs an exec.Cmd for a gcloud command,
