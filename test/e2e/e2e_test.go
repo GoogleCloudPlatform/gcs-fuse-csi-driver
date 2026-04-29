ae092aa83437cb201bc625eecc7a51c825cacb31
samyukthapa <samyukthapa@google.com>
2026-04-29 18:25:49 +0530

    misc-fix

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -125,6 +125,7 @@ var _ = ginkgo.Describe("E2E Test Suite", func() {
 			testsuites.InitGcsFuseCSIMetadataPrefetchTestSuite,
 			testsuites.InitGcsFuseMountTestSuite,
 			testsuites.InitGcsFuseCSIOIDCTestSuite,
+			testsuites.InitGcsFuseCSICloudProfilerTestSuite,
 			testsuites.InitGcsFuseCSIWorkloadIdentityFederationTestSuite,
 		}
 
