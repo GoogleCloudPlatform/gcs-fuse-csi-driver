9a0b0cf0d3d217e609731b6fcbc0fcab3ea393e7
Sneha Aradhey <snehaaradhey@google.com>
2026-06-08 20:09:44 +0000

    test updates based on review comments

--- pkg/sidecar_mounter/sidecar_mounter_config.go
+++ pkg/sidecar_mounter/sidecar_mounter_config.go
@@ -114,10 +114,6 @@ func NewMountConfig(sp string, flagMapFromDriver map[string]string) *MountConfig
 		ErrWriter:  NewErrorWriter(filepath.Join(tempDir, util.ErrorFileName)),
 	}
 
-	if err := mc.ErrWriter.Clean(); err != nil {
-		klog.Warningf("failed to cleanup stale error file: %v", err)
-	}
-
 	klog.Infof("connecting to socket %q", sp)
 	c, err := net.Dial("unix", sp)
 	if err != nil {
